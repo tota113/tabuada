127d6dfb4f6c10f2b8b688eac35ae8c5a124451b
tota113 <105284820+tota113@users.noreply.github.com>
2022-05-25 09:51:20 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function tabuada(){
+     var num = parseInt(document.getElementById("num").value);
+     var resultado = document.getElementById("resposta");
+     var tabuada = '';
+
+      for(var count = 1; count <= 10; count ++){
+           tabuada += num+ "x" +count+ "="+
+        num*count+"<br/>"
+
+        resultado.innerHTML = tabuada;
+      }
+
+}
